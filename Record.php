8f906b7ce2695c0755bb56b2816b297c444752dc
PSmith4 <7191731@student.swin.edu.au>
2016-09-04 12:05:53 +1000

    database address fix

--- Record.php
+++ Record.php
@@ -11,7 +11,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$callState = "SELECT Name, BirthDate, EntryDate, AgeRecorded FROM agecalcsql1.record_of_request;";
+$callState = "SELECT Name, BirthDate, EntryDate, AgeRecorded FROM record_of_request;";
 
 
 $data=$conn->query($callState);
