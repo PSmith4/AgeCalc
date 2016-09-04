1c7baa5178ad43da28e131f0530ffa8cfc334cf1
PSmith4 <7191731@student.swin.edu.au>
2016-09-04 11:37:07 +1000

    record.php

---
+++ Record.php
@@ -0,0 +1,33 @@
+<?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$servername = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$dbname =  substr($url["path"], 1);
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$insertState = "SELECT Name, BirthDate, EntryDate, AgeRecorded FROM agecalcsql1.record_of_request;";
+
+
+$data=$conn->query($insertState);
+
+$conn->close();
+
+echo '<table> 
+	<tr>
+    <th>Name</th>
+    <th>Entered Birtday</th> 
+    <th>Date Entered</th>
+	<th>Age Reported</th>
+	</tr>'
+
+foreach ($data as $row){
+	echo '<tr> <td> $row["Name"] </td> <td> $row["BirthDate"] </td> <td> $row["EntryDate"] </td> <td> $row["AgeRecorded"] </td> </tr>'
+}
+	
+echo '</table>'	
+ ?>
