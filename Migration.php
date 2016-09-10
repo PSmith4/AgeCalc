528f838be3fb3d1964936ad056f2aaa586ca9695
PSmith4 <7191731@student.swin.edu.au>
2016-09-10 19:00:09 +1000

    asdf

--- Migration.php
+++ Migration.php
@@ -1,3 +1,14 @@
 <?php
 echo "start";
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$servername = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$dbname =  substr($url["path"], 1);
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "connected";
 ?>
