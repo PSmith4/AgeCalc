dbef7fc5c5016906dc8f7ffa77903efbff7d5830
PSmith4 <7191731@student.swin.edu.au>
2016-09-10 18:26:34 +1000

    V2 with migration test

--- Migration.php
+++ Migration.php
@@ -1,4 +1,5 @@
 <?php
+
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $servername = $url["host"];
 $username = $url["user"];
@@ -29,20 +30,18 @@ insert into record_of_request2 (FName, BirthDate, EntryDate, AgeRecorded)
 Select Name, BirthDate, EntryDate, AgeRecorded FROM record_of_request;"
 
 $conn->query($SQLStatment);
-
+echo "Tabel Recreation done";
 
 $callState = "SELECT ID, FName FROM record_of_request2;";
 $data=$conn->query($callState);
 foreach ($data as $row){
-	$names explode(' ', $row.['FName']);
-	$SqlStatment =
-	"	UPDATE record_of_request2
-		SET FName=$names[0], SName=$names[1]
-		WHERE ID=$row.['ID'] ";
+	echo "$row['FName']";
+	$names explode(' ', $row['FName']);
+	$SqlStatment ="
+	UPDATE record_of_request2
+	SET FName=$names[0], SName=$names[1]
+	WHERE ID=$row['ID'];";
 	$conn->query($SQLStatment);
 }
-
-
-
 
 ?>
