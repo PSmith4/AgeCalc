8def8732fadd1905f593feb4ba94ff76fb469b02
PSmith4 <7191731@student.swin.edu.au>
2016-09-10 18:58:06 +1000

    V2 with migration test

--- Migration.php
+++ Migration.php
@@ -11,22 +11,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$SQLStatment =
-"DROP TABLE IF EXISTS `record_of_request2`;
-
-CREATE TABLE `record_of_request2` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `FName` mediumtext,
-  `SName` mediumtext,
-  `BirthDate` datetime DEFAULT NULL,
-  `EntryDate` datetime DEFAULT NULL,
-  `AgeRecorded` int DEFAULT NULL,
-  `MarsAgeRecorded` int DEFAULT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
-
-insert into record_of_request2 (FName, BirthDate, EntryDate, AgeRecorded)
-Select Name, BirthDate, EntryDate, AgeRecorded FROM record_of_request;"
+$SQLStatment ="DROP TABLE IF EXISTS `record_of_request2`;CREATE TABLE `record_of_request2` (`ID` int NOT NULL AUTO_INCREMENT, `FName` mediumtext,  `SName` mediumtext,  `BirthDate` datetime DEFAULT NULL,  `EntryDate` datetime DEFAULT NULL,`AgeRecorded` int DEFAULT NULL,`MarsAgeRecorded` int DEFAULT NULL, PRIMARY KEY (`ID`)) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8; insert into record_of_request2 (FName, BirthDate, EntryDate, AgeRecorded) Select Name, BirthDate, EntryDate, AgeRecorded FROM record_of_request;"
 echo $SQLStatment
 $conn->query($SQLStatment);
 echo "Tabel Recreation done";
