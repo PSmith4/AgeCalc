c3323736727ff12ef1e8dbb775e9e28eb9befdc0
PSmith4 <7191731@student.swin.edu.au>
2016-09-10 18:55:16 +1000

    V2 with migration test

--- Migration.php
+++ Migration.php
@@ -25,23 +25,10 @@ CREATE TABLE `record_of_request2` (
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
 
-
 insert into record_of_request2 (FName, BirthDate, EntryDate, AgeRecorded)
 Select Name, BirthDate, EntryDate, AgeRecorded FROM record_of_request;"
 echo $SQLStatment
 $conn->query($SQLStatment);
 echo "Tabel Recreation done";
-
-$callState = "SELECT ID, FName FROM record_of_request2;";
-$data=$conn->query($callState);
-foreach ($data as $row){
-	echo $row['FName'];
-	$names explode(' ', $row['FName']);
-	$SqlStatment ="
-	UPDATE record_of_request2
-	SET FName=$names[0], SName=$names[1]
-	WHERE ID=$row['ID'];";
-	$conn->query($SQLStatment);
-}
 
 ?>
