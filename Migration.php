03ae22759c3bd060041ee904762d5fe8027c3b0d
PSmith4 <7191731@student.swin.edu.au>
2016-09-10 18:59:08 +1000

    adsfg

--- Migration.php
+++ Migration.php
@@ -1,19 +1,3 @@
 <?php
 echo "start";
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$servername = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$dbname =  substr($url["path"], 1);
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-
-$SQLStatment ="DROP TABLE IF EXISTS `record_of_request2`;CREATE TABLE `record_of_request2` (`ID` int NOT NULL AUTO_INCREMENT, `FName` mediumtext,  `SName` mediumtext,  `BirthDate` datetime DEFAULT NULL,  `EntryDate` datetime DEFAULT NULL,`AgeRecorded` int DEFAULT NULL,`MarsAgeRecorded` int DEFAULT NULL, PRIMARY KEY (`ID`)) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8; insert into record_of_request2 (FName, BirthDate, EntryDate, AgeRecorded) Select Name, BirthDate, EntryDate, AgeRecorded FROM record_of_request;"
-echo $SQLStatment
-$conn->query($SQLStatment);
-echo "Tabel Recreation done";
-
 ?>
