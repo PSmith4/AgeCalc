57477d0e7caae975df0d299b2fbb68ccca46ae23
PSmith4 <7191731@student.swin.edu.au>
2016-09-11 10:34:20 +1000

    )

--- Migration.php
+++ Migration.php
@@ -13,10 +13,11 @@ if ($conn->connect_error) {
 echo "connected\r\n";
 
 $SQLStatment = "DROP TABLE IF EXISTS `record_of_request2`;"; 
-if ($conn->query($SQLStatment) === TRUE){ echo "Drop Done";} else {echo "Error dropping " . $conn->error;}
-}
+if ($conn->query($SQLStatment) === TRUE){ echo "Drop Done";} 
+else {echo "Error dropping " . $conn->error;}
 $SQLStatment ="CREATE TABLE `record_of_request2` (  `ID` int NOT NULL AUTO_INCREMENT,  `FName` mediumtext,  `SName` mediumtext,  `BirthDate` datetime DEFAULT NULL,  `EntryDate` datetime DEFAULT NULL,  `AgeRecorded` int DEFAULT NULL,  `MarsAgeRecorded` int DEFAULT NULL,  PRIMARY KEY (`ID`)) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8; INSERT INTO record_of_request2 (FName, BirthDate, EntryDate, AgeRecorded) Select Name, BirthDate, EntryDate, AgeRecorded FROM record_of_request;";
-if ($conn->query($SQLStatment) === TRUE){ echo "Tabel Recreation don";} else {echo "Error recreating " . $conn->error;}
+if ($conn->query($SQLStatment) === TRUE){ echo "Tabel Recreation don";} 
+else {echo "Error recreating " . $conn->error;}
 
 $callState = "SELECT ID, FName FROM record_of_request2;";
 $data=$conn->query($callState);
@@ -34,7 +35,8 @@ foreach ($data as $row){
 		echo "|";
 		$SqlStatment = "UPDATE record_of_request2 SET FName='" . $names[0] . "' , SName= '". $names[1] ."' WHERE ID= ". $row['ID'] . ";";
 		echo $SqlStatment;
-		if ($conn->query($SQLStatment) === TRUE){ echo "Update done";} else {echo "Error updateing " . $conn->error;}
+		if ($conn->query($SQLStatment) === TRUE){ echo "Update done";} 
+		else {echo "Error updateing " . $conn->error;}
 	}
 }
 $conn->close();
