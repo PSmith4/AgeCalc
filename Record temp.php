a12872299de178c1510416e754cdd972a0b41499
PSmith4 <7191731@student.swin.edu.au>
2016-10-02 15:14:17 +1100

    asdf

--- Record temp.php
+++ Record temp.php
@@ -4,20 +4,22 @@ $username = "ageCalc";
 $password = "password";
 $dbname =  "ageCalcRecords";
 
-$conn = mysql_connect($servername, $username, $password);
-mysql_select_db($dbname, $conn);
-
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
 
 $callState = "SELECT Name, BirthDate, EntryDate, AgeRecorded FROM record_of_request;";
 
 
-$data=mysql_query($callState,$conn) or die("Query $callState failed ".mysql_error($conn));
-$numRows = mysql_num_rows($data) or die ('couldnt count rows'.mysql_error($conn));
+$data=$conn->query($callState) or die("Query $callState failed ".mysqli_error($conn));
+$numRows = $data->num_rows();
 echo $numRows;
 if(empty($data)){
 	echo "nothing recived";
 }
 
+$conn->close();
 
 
 foreach ($data as $row){
